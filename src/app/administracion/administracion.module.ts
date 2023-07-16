c3a73e0f55c3ee6b854806268f9f378852f4be12
Viviana Ramirez <ramirezvivi0102@gmail.com>
2023-07-16 18:41:58 -0500

    agrega modulos de componentsModule y copia ejemplo de tableComponente (codigo) en tipo-vehiculoComp

--- src/app/administracion/administracion.module.ts
+++ src/app/administracion/administracion.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ComponentsRoutes } from '../component/component.routing';
 import { AdministracionRoutes } from './administracion.routing';
+import { ComponentsModule } from '../component/component.module';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    ComponentsModule,
+    NgApexchartsModule,
     RouterModule.forChild(AdministracionRoutes),
   ],
   // declarations: [
--- src/app/administracion/tipo-vehiculo/tipo-vehiculo.component.ts
+++ src/app/administracion/tipo-vehiculo/tipo-vehiculo.component.ts
@@ -1,10 +1,23 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { Employee, Product, TableRows, TopSelling } from 'src/app/component/table/table-data';
 
 @Component({
   selector: 'app-tipo-vehiculo',
   templateUrl: './tipo-vehiculo.component.html',
-  styleUrls: ['./tipo-vehiculo.component.scss']
+  styleUrls: ['./tipo-vehiculo.component.scss'],
+  standalone: true,
+  imports:[NgFor]
 })
 export class TipoVehiculoComponent {
+  topSelling: Product[];
 
+  trow: TableRows[];
+
+  constructor() {
+
+    this.topSelling = TopSelling;
+
+    this.trow = Employee;
+  }
 }
